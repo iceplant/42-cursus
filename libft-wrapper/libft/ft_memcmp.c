091598d29ba62ac616f093329d92b7058608ec78
Rocky Kamen-Rubio <rokamen-@car6s3.42barcelona.com>
2023-09-19 12:35:55 +0200

    adds memcmp.c

---
+++ libft-wrapper/libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 12:28:32 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/09/19 12:35:25 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*cs1;
+	unsigned char	*cs2;
+	size_t			i;
+
+	cs1 = (unsigned char *)s1;
+	cs2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (cs1[i] != cs2[i])
+			return (cs1[i] - cs2[i]);
+		i++;
+	}
+	return (0);
+}
