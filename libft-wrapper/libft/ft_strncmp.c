1ad7cab9d637a72a73f274aee2368f5d0041af25
Rocky Kamen-Rubio <rokamen-@car6s3.42barcelona.com>
2023-09-19 13:50:54 +0200

    strncmp

---
+++ libft-wrapper/libft/ft_strncmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 13:30:43 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/09/19 13:50:14 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	if (i == n)
+		return (0);
+	return (((unsigned char)s1[i]) - ((unsigned char)s2[i]));
+}
