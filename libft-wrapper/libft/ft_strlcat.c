b4f1c7f72d01919357b1b54ecba036026d43066f
Rocky Kamen-Rubio <rokamen-@cbr10s3.42barcelona.com>
2023-09-20 16:18:08 +0200

    working strlcat

---
+++ libft-wrapper/libft/ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/19 14:36:14 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/09/20 16:17:55 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlen(char *str);
+
+size_t ft_strlcat(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+	size_t	i;
+	size_t	srclen;
+	size_t	dstlen;
+
+	srclen = ft_strlen((char *) src);
+	if (dstsize == 0)
+		return (srclen + dstsize);
+	dstlen = ft_strlen((char *) dst);
+	if (dstlen  >= dstsize)
+		return (srclen + dstsize);
+	i = 0;
+	while (src[i] && (dstlen + i) < dstsize - 1)
+	{
+		dst[dstlen + i] = src[i];
+		i++;
+	}
+	dst[dstlen + i] = '\0';
+	return (srclen + dstlen);
+}
