0f89824da886ce365380a13ce3ecb697ba923cf6
Rocky Kamen-Rubio <rokamen-@cbr7s3.42barcelona.com>
2023-09-21 16:05:34 +0200

    strjoin.c

---
+++ libft-wrapper/libft/ft_strjoin.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rokamen- <rokamen-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 15:05:43 by rokamen-          #+#    #+#             */
+/*   Updated: 2023/09/21 16:05:23 by rokamen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t	ft_strlen(const char *s);
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+	int	s1_len;
+	int	s2_len;
+	char	*rtn;
+	int	i;
+	
+	if (!s1 && !s2)
+		return (0);
+
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	rtn = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (!rtn)
+		return (NULL);
+	i = 0;
+	while (i < s1_len + s2_len)
+	{
+		if (i < s1_len)
+			rtn[i] = s1[i];
+		else
+		   rtn[i] = s2[i - s1_len];
+		i++;	
+	}
+	rtn[i] = '\0';
+	return (rtn);
+}
