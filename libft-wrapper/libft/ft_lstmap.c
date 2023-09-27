c75fd2bbe15b20ea6e85b2158f2997d7a297c9d3
Rocky Kamen-Rubio <rokamen-@cbr14s6.42barcelona.com>
2023-09-27 17:01:20 +0200

    everything working except lstmap

--- libft-wrapper/libft/ft_lstmap.c
+++ libft-wrapper/libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rokamen- <rokamen-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 16:14:45 by rokamen-          #+#    #+#             */
-/*   Updated: 2023/09/27 16:32:18 by rokamen-         ###   ########.fr       */
+/*   Updated: 2023/09/27 16:54:23 by rokamen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*rtn;
-	t_list	*temp;
+	t_list	*new_element;
 
 	if (!lst)
 		return (NULL);
 	while (lst)
 	{
-		temp = ft_lstnew(f(lst->content));
-		if (!temp)
+		new_element = ft_lstnew(f(lst->content));
+		if (!new_element)
 		{
-			ft_lstclear(&rtn, del);
+			ft_lstclear(&rtn, (*del));
 			return (NULL);
 		}
-		ft_lstadd_back(&rtn, temp);
+		ft_lstadd_back(&rtn, new_element);
 		lst = lst->next;
 	}
 	return (rtn);
